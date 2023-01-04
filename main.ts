3bb99acc8ee952a721d848be29296b2dafd1d82d
Crispin Bennett <myself@crisbennett.com>
2023-01-04 21:44:14 +1100

    Add skeleton runner

--- main.ts
+++ main.ts
@@ -1,24 +1,34 @@
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+import { readAllSync } from "https://deno.land/std@0.170.0/streams/read_all.ts";
+import { writeAllSync } from "https://deno.land/std@0.170.0/streams/write_all.ts";
+
 if (import.meta.main) {
-    main(Deno.args);
+  main(Deno.args);
 }
 
 function main(args: string[]) {
-    if (args.length > 1) {
-        console.log("Usage: deno jlox [filename]");
-        Deno.exit(64);
-    } else if (args.length == 1) {
-        runFile(args[0]);
-    } else {
-        runPrompt();
-    }
-
+  if (args.length > 1) {
+    console.log("Usage: deno jlox [filename]");
+    Deno.exit(64);
+  } else if (args.length == 1) {
+    runFile(args[0]);
+  } else {
+    runPrompt();
+  }
 }
 
-function runFile(fileName: string){
-    console.log(`Run file ${fileName} TODO`);
+function runFile(filePath: string) {
+    const source = Deno.readTextFileSync(filePath);
+    run(source);
 }
 
-function runPrompt(){
-    console.log("Run prompt TODO");
+function runPrompt() {
+  const prompt = new TextEncoder().encode("> ");
+  writeAllSync(Deno.stdout, prompt);
+  const buffer = readAllSync(Deno.stdin);
+  const thingo: string = new TextDecoder().decode(buffer);
+  run(thingo);
+}
+
+function run(source: string) {
+  console.log(`running code:\n\n${source}`);
 }

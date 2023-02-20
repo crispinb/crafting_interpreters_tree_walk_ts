5946ce2007823c350e9ed179d822802bf234e76a
Crispin Bennett <myself@crisbennett.com>
2023-02-20 11:18:06 +1100

    Rearrange class generation
    - realised that we need the created class file for mainstream
      use, not just as a post-compile 'generated' entity

--- main_test.ts
+++ main_test.ts
@@ -2,24 +2,7 @@
 import Scanner, { Error } from "./scanner.ts";
 import Token from "./token.ts";
 
-Deno.test(function basicScan() {
-  const source = Deno.readTextFileSync("./temp_test.lox");
-  const scanner = new Scanner(source);
+// Deno.test(function basicScan() {
 
-  const [tokens, errors] = scanner.scanTokens();
+// });
 
-  console.log("\nTokens: \n");
-  display_arr(tokens);
-  console.log("\nErrors: \n");
-  display_arr(errors);
-
-  // length has to account for extra EOF token
-  // assertEquals(tokens.length, 7);
-  // assertEquals(errors.length, 5);
-});
-
-function display_arr(tokens: Array<Token> | Array<Error>) {
-  for (const token of tokens) {
-    console.log(`${JSON.stringify(token)}`);
-  }
-}
